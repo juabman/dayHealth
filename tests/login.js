aa7cc77a257a0a1e05621dbffe588f62ae47030e
Scott Taylor <juabman@hotmail.com>
2019-04-19 08:51:18 -0600

    Updates to Pauses

--- tests/login.js
+++ tests/login.js
@@ -12,10 +12,10 @@ module.exports = {
     },
     //test for signing up, signing in, and username taken
     'SignUp, signIn, and username taken': browser => {
-        dayHealthLogin.signUp('Tony Stark2', 'tstark2', 'Tony2@avengers.com', 'Ironman2')
+        dayHealthLogin.signUp('Tony Stark11', 'tstark11', 'Tony11@avengers.com', 'Ironman11')
     },
     //compose entry
     'Composing an entry': browser => {
-        dayHealthLogin.compose('tstark2', 'Ironman2', 'Test Entry', 'I am feeling great today!')
+        dayHealthLogin.compose('tstark11', 'Ironman11', 'Test Entry', 'I am feeling great today!')
     },
 }
